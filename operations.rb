616d8d113758b7e9ff2922438aa78d549057ecc9
Katie Hoffman <ktahoffman@gmail.com>
2014-09-24 11:12:33 -0400

    add empty methods

--- operations.rb
+++ operations.rb
@@ -1,13 +1,18 @@
-#use an if else statement to define whether the conditions are unsafe (speed is less than 40 or speed is greater than 60 )
+def unsafe?(speed)
+  # use an if else statement to define whether the conditions are unsafe (speed is less than 40 or speed is greater than 60 )
+end
 
+def not_safe?(speed)
+  # use a ternary operator to define whether the conditions are unsafe
+end
 
-#use a ternary operator to define whether the conditions are unsafe
+def sum_machine(first_num, second_num)
+  # build a method named sum_machine that takes two args and returns the sum of all the numbers in between
+end
 
-
-#build a one line method named sum_machine that takes two args and returns the sum of all the numbers in between
-
-
-#given the array
-crazy_sauce = [nil, 2, 4, 5, 6, nil, 10]
-#output the squares of each number. For nil output 0.
-#You cannot use the method #compact but you should use the ||= operator
+def squares(collection)
+  # given the array
+  # crazy_sauce = [nil, 2, 4, 5, 6, nil, 10]
+  # output the squares of each number. For nil output 0.
+  # try not to use the #compact but you should use the ||= operator
+end
