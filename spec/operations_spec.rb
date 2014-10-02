4f6831f88b4e6c2075640979c97dd41a8c34fa47
Katie Hoffman <ktahoffman@gmail.com>
2014-10-02 13:07:04 -0400

    change strings into t/f

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,36 +1,36 @@
 describe "unsafe? using an if else block" do
   it 'returns true if speed is greater than 60' do
     answer = unsafe?(79)
-    expect(answer).to eq("unsafe")
+    expect(answer).to eq(true)
   end
 
   it 'returns true if speed is less than 40' do
     answer = unsafe?(35)
-    expect(answer).to eq("unsafe")
+    expect(answer).to eq(true)
   end
 
   it 'returns false if the speed is between 40 and 60' do
     answer = unsafe?(50)
 
-    expect(answer).to eq("safe")
+    expect(answer).to eq(false)
   end
 end
 
 describe "not_safe? using the ternary operator" do
   it 'returns true if speed is greater than 60' do
     answer = not_safe?(79)
-    expect(answer).to eq("unsafe")
+    expect(answer).to eq(true)
   end
 
   it 'returns true if speed is less than 40' do
     answer = not_safe?(35)
-    expect(answer).to eq("unsafe")
+    expect(answer).to eq(true)
   end
 
   it 'returns false if the speed is between 40 and 60' do
     answer = not_safe?(50)
 
-    expect(answer).to eq("safe")
+    expect(answer).to eq(false)
   end
 
   it 'uses the ternary operator' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 # specs live under a `spec` directory, which RSpec adds to the `$LOAD_PATH`.
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
-require_relative './spec_helper'
 require_relative '../operations.rb'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
