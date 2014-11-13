4a74efa87c343d378ce1814fda26b4beeb4b3958
Katie Hoffman <ktahoffman@gmail.com>
2014-11-13 15:45:52 -0500

    not passing two specs

--- spec/02_cli_spec.rb
+++ spec/02_cli_spec.rb
@@ -13,16 +13,46 @@ describe "Part Two - CLI" do
   describe "#print_methods" do
     it "prints out 'All Methods' followed by a list of every method name" do
       output = capture_stdout { print_methods }
-      expect(output).to include("All Methods:")
+      expect(output).to match("All Methods:")
       method_names.each { |name| expect(output).to include(name) }
     end
   end
 
+  # unsafe?
+  # not_safe?
+  # sum_machine
+  # square
+
   describe "#main" do
-    it "prints out a prompt to type in a method name" do
+    
+    it "prompts user to type in a method name or to quit" do
+      allow(self).to receive(:gets).and_return("unsafe?")
+      output = capture_stdout { main }
+      expect(output).to include("Type the name of the method you would like to use, type q to quit:")
+    end
+
+    it "prompts user for number when they enter 'unsafe?'" do
+      allow(self).to receive(:gets).and_return("unsafe?")
+      output = capture_stdout { main }
+      expect(output).to include("Type a number to give to unsafe?:")
+    end
+
+    it "prompts user for number when they enter 'not_safe?'" do
+      allow(self).to receive(:gets).and_return("not_safe?")
+      output = capture_stdout { main }
+      expect(output).to include("Type a number to give to not_safe?:")
+    end
+
+    it "prompts user for two comma separated numbers when they enter 'sum_machine'" do
+      allow(self).to receive(:gets).and_return("sum_machine")
+      output = capture_stdout { main }
+      expect(output).to include("Type a number to give to sum_machine:")
+    end
+
+    it "prints out a 'not recognized' error when user types in a method name that does not exist" do
       allow(self).to receive(:gets).and_return("incorrect_method_name")
       output = capture_stdout { main }
-      expect(output).to match("Type the name of the method you would like to use, type q to quit:")
+      expect(output).to include("Method name not recognized.")
     end
   end
 
