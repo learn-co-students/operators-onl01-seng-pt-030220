d4e2ed0279236b65e0b3172a6d24ce4262a44478
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-06-25 14:39:31 -0400

    fix readme, fix spec

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,11 @@
 def unsafe?(speed)
-	if speed > 60 
-		true
-	elsif 
-		speed < 40
-		true
-	else
-		false
-	end
+
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? true : false
+	
 end
 	
 
