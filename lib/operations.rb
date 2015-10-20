8afeab04e314ae2124e3d901a3395e2c8a5e22fa
Deniz Unat <deniznida@users.noreply.github.com>
2015-10-20 11:21:41 -0400

    Update operations.rb

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-	if speed > 60 
-		true
-	elsif 
-		speed < 40
-		true
-	else
-		false
-	end
+  if speed > 60 
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 end
 
 def not_safe?(speed)
