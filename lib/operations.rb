ba0ee94a3dd13d4fec7272c704f9be64bbc6f3d8
Bikram Sodhi <biksodhi@gmail.com>
2020-03-05 17:10:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 60 || speed < 40 ? true : false
+
 end
-	
-
-
