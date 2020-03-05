c0a99052b0a3c223cb2c4711820b86b319adfbac
Ty Raino <mstymetris@yahoo.com>
2020-03-05 00:06:35 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+	if speed > 60 || speed < 40
+		return true
+	else
+		return false
+	end
 end
-
 
 
 def not_safe?(speed)
-	
+speed < 40 || speed > 60 ? true : false
 end
-	
-
-
