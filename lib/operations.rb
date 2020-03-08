dc52424416bf3338c404bfdab4d2fe7063955842
Travis Gandy <travis_gandy@yahoo.com>
2020-03-08 06:53:59 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-
+  if speed < 40
+    true 
+  elsif speed > 60
+   true
+  else
+    false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+  
+	speed > 60 || speed < 40 ? true : false
+
+
 end
-	
-
-
