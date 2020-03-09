22b7fa776b95d825f43c6ceaff2dafa2680be00c
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 02:00:41 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
+#if speed 40> or 60< its unsafe. 
+
 def unsafe?(speed)
+  if speed <40 || speed > 60
+    return true 
+  else 
+    return false 
+  end 
+end 
 
-end
+#if speed 40> or 60< its safe. 
 
+ def not_safe?(speed)
+    speed < 40  || speed > 60  ? true : false
+ end
 
-
-def not_safe?(speed)
-	
-end
 	
 
 
