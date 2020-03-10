e81b9499ba2a325bb7535386455b885ffd8c80e5
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-10 08:34:26 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,11 @@
 def unsafe?(speed)
   
-  if (speed < 40 || speed > 60)
-    return "true"
-  else 
-    "false"
-     
+  if speed < 40
+    return true
+  elsif speed > 60
+    true
+  else
+    false
   end
 
 end
@@ -12,7 +13,7 @@ end
 
 
 def not_safe?(speed)
-	(speed < 40 || speed  > 60) ? "true" : "false"
+	(speed < 40 || speed  > 60) ? true : false
 end
 	
 
