69af045fa04605af71f6d9defe59964c8b0988d8
Colby Edrington <ck.edrington@gmail.com>
2020-03-11 01:09:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
+## inputs --> speed -- number(integer)
+## outputs --> answer the question am i unsafe --> boolean
+
+
+#if your speed is > 60 or your speed is < 40, say true
+#otherwise, say false
+
 def unsafe?(speed)
-
+	if speed > 60 || speed < 40
+		return true
+	else
+		return false
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
