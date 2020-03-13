fa2d559a1d00bcd530e00439d806e44558c0bbca
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-13 21:59:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+if speed < 40
+  true
+elsif speed > 60
+  true
+else
+  false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+speed < 40? true : false
+speed > 60? true : false
+speed > 40 && speed < 60? false : true
 end
-	
-
-
