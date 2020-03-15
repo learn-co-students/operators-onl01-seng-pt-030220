d64f987e6b850b38552f424fd6c02fc20c29e484
Kimberly Aviles <01010110.v@gmail.com>
2020-03-15 20:34:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,19 @@
 def unsafe?(speed)
-speed > 60 || speed < 40 ? true : false
+
+	if speed < 40
+		true
+	elsif speed > 60
+		true
+	else
+		false
+	end
+
 end
 
 
 
 def not_safe?(speed)
-speed > 60 || speed < 40 ? true : false
+
+	speed > 60 || speed < 40 ? true : false
+
 end
